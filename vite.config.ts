ab97e04d18fbbe88027874c72e54b9f1fcdaf40e
sona9090 <sonasarkisian90@gmail.com>
2026-08-27 19:58:23 +0400

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { sites } from '@openai/sites-vite-plugin';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), sites()],
-});
+  plugins: [react()],
+  base: '/atmos-weather/',
+})
